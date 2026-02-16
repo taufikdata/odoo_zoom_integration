e40e446537a547346d2be0399564a6a1dceb5585
taufikdata <taufikhidayatbaus@gmail.com>
2026-02-16 15:04:31 +0800

    revisi om container final

--- custom_addons/om_container_tracker/__manifest__.py
+++ custom_addons/om_container_tracker/__manifest__.py
@@ -3,13 +3,15 @@
     'version': '1.0',
     'summary': 'Track Container Position via TimeToCargo API',
     'author': 'Taufik Hidayat',
-    'depends': ['stock', 'website', 'sale'], # Kita butuh modul stock (inventory)
+    'depends': ['stock', 'website', 'sale'],
     'data': [
         'security/ir.model.access.csv',
+        'data/api_credentials.xml',
         'data/container.tracking.status.csv',
         'views/tracking_status_views.xml',
-        'views/sale_order_views.xml',
+        # 'views/sale_order_views.xml',
         'views/tracking_template.xml',
+        'views/audit_views.xml',
     ],
     'installable': True,
     'application': False,
